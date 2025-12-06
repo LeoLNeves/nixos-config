4560ceb17480237e1507f59270f01bcf6c76b26e
Leonardo Neves <leonardo.neves@tecnico.ulisboa.pt>
2025-12-06 18:44:49 +0000

    The beggining of my NixOs configuration journey

---
+++ README.md
@@ -0,0 +1,8 @@
+# Initial Version of my NixOS configuration
+
+This is where it begins. This version will only contain the current config and hardware config from /etc/nixos
+
+This is a very simple config without the use flakes or home manager yet.
+
+This config can be run with: "sudo nixos-rebuild switch -I nixos-config=/PATH_TO/configuration.nix"
+
---
+++ configuration.nix
@@ -0,0 +1,87 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  networking.networkmanager.dns = "none";
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  networking.hostName = "shire";
+  networking.networkmanager.enable = true;
+
+  services.openssh.enable = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  services.xserver.enable = true;
+
+  time.timeZone = "Europe/Lisbon";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_PT.UTF-8";
+    LC_IDENTIFICATION = "pt_PT.UTF-8";
+    LC_MEASUREMENT = "pt_PT.UTF-8";
+    LC_MONETARY = "pt_PT.UTF-8";
+    LC_NAME = "pt_PT.UTF-8";
+    LC_NUMERIC = "pt_PT.UTF-8";
+    LC_PAPER = "pt_PT.UTF-8";
+    LC_TELEPHONE = "pt_PT.UTF-8";
+    LC_TIME = "pt_PT.UTF-8";
+  };
+ 
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.xkb = {
+    layout = "pt";
+    variant = "";
+  };
+
+  console.keyMap = "pt-latin1";
+
+  services.printing.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.leo = {
+    isNormalUser = true;
+    description = "Leonardo";
+    extraGroups = [ "networkmanager" "wheel"];
+    packages = with pkgs; [
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+  brave
+  mattermost-desktop
+  discord
+  neovim
+  obsidian
+  spotify
+  git
+  vscode
+  docker 
+  git
+  rclone
+  zip
+  ];
+
+  system.stateVersion = "25.05"; 
+
+}
