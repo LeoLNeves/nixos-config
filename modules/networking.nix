9c49b6a33d5eaee2f718b6fa8be42a7877d724ad
Leonardo Neves <leonardo.neves@tecnico.ulisboa.pt>
2025-12-24 00:09:21 +0000

    add host variable to networking

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,7 +1,7 @@
-{ ... }:
+{ host, ... }:
 {
     networking.networkmanager.dns = "none";
     networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
-    networking.hostName = "shire";
+    networking.hostName = host;
     networking.networkmanager.enable = true;
 }
