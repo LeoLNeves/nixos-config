badcea1d71919ee2dddb6af4102c7c05a7e16511
Leonardo Neves <leonardo.neves@tecnico.ulisboa.pt>
2026-01-07 19:12:42 +0000

    change hostname for virtus

--- hosts/virtus/default.nix
+++ hosts/virtus/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, host, ... }:
 
 {
   imports =
@@ -16,7 +16,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = host; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
