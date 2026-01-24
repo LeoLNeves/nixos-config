6d19e2cf4bd400bc301a8d1b3fa3a80950e72a94
Leonardo Neves <leonardo.neves@tecnico.ulisboa.pt>
2026-01-24 23:21:52 +0000

    create jellyfin user

--- modules/server/jellyfin.nix
+++ modules/server/jellyfin.nix
@@ -4,5 +4,8 @@
     services.jellyfin = {
         enable = true;
         openFirewall = false; # caddy handles this
+        user = "jellyfin";
     };
+
+    users.users.jellyfin.extraGroups = [ "users" "video" "render" ];
 }
