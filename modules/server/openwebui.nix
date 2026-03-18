5eedd00b6dc7eb8bb2134ad8fc422b3454ba7ac7
Leonardo Neves <leonardo.neves@tecnico.ulisboa.pt>
2026-03-18 21:29:44 +0000

    more flags to open web ui

--- modules/server/openwebui.nix
+++ modules/server/openwebui.nix
@@ -6,6 +6,15 @@
     port = 7860;
     environment = {
       DEFAULT_PROMPT_SUGGESTIONS = "false";
+
+      ENABLE_EASTER_EGGS = "False";
+      ENABLE_USER_STATUS = "False";
+      ENABLE_TAGS_GENERATION = "False";
+      ENABLE_CHANNELS = "False";
+      
+      ENABLE_SIGNUP = "Olympia";
+      WEBUI_NAME = "";
+      ENABLE_PERSISTENT_CONFIG = "False";
     };
   };
 }
